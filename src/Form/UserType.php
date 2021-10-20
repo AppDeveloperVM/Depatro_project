508647f6554de962cd07e2a475576f34b7d667bd
AppDeveloperV <vicmr1994@gmail.com>
2021-10-21 00:04:09 +0200

    Update UserType.php

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,10 +15,11 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
+            ->add('email', EmailType::class)
+            // ->add('roles')
+            ->add('password',PasswordType::class)
             ->add('nombre')
+            ->add('Registrar',SubmitType::class)
         ;
     }
 
