067c61c40d2df8f6bcb2ed08a7ade8ee887f0143
AppDeveloperV <vicmr1994@gmail.com>
2021-10-21 19:20:20 +0200

    js ajax-call updated

--- public/js/ajax-call.js
+++ public/js/ajax-call.js
@@ -1,12 +1,30 @@
-function Login(){
+function Login(email,passwd){
+    var Ruta ="/login";
+
+    // pass auth vars to Security (Login) Controller
+
     $.ajax({
          type: 'POST',
          url: Ruta,
-         data: ({id: id}),
+         data: ({email: email,passwd: passwd}),
          async: true,
          dataType:"json",
-         success: function (data){
- 
-         }
+
+         /*success: function (data){
+            //Login succesful, reload vars
+         }*/
+         success: function(data, status) {  
+            //success example, modify template info
+
+            //$('..').append(e); $('#..').html('');
+             
+            $('#login_info').html(data);  // data.username , data.passwd 
+             
+            
+            
+         },  
+         error : function(xhr, textStatus, errorThrown) {  
+            alert('Ajax login request failed.');  
+         }  
      });
 }
