87bd8b1463a0069d1cb39cbe103357963e898517
AppDeveloperV <vicmr1994@gmail.com>
2021-10-21 20:13:50 +0200

    js ajax-call updated

--- public/js/ajax-call.js
+++ public/js/ajax-call.js
@@ -1,27 +1,27 @@
 function Login(email,passwd){
     var Ruta ="/login";
-
     // pass auth vars to Security (Login) Controller
+
+    //get form values
+
+    /*
+    var email = $('#email').val();
+    var passwd = $('#passwd').val();
+    */
+    //({email: email,passwd: passwd}
 
     $.ajax({
          type: 'POST',
          url: Ruta,
-         data: ({email: email,passwd: passwd}),
+         data: $("#login_form").serialize(),
          async: true,
          dataType:"json",
-
-         /*success: function (data){
-            //Login succesful, reload vars
-         }*/
          success: function(data, status) {  
             //success example, modify template info
-
             //$('..').append(e); $('#..').html('');
-             
-            $('#login_info').html(data);  // data.username , data.passwd 
-             
-            
-            
+
+            //$('#login_info').html(data);  // data.username , data.passwd               
+            alert('Ajax login request!');
          },  
          error : function(xhr, textStatus, errorThrown) {  
             alert('Ajax login request failed.');  
