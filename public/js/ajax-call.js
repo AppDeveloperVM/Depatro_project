0aeb77181d202d272a423cc6225e6941370e4b52
AppDeveloperV <vicmr1994@gmail.com>
2021-10-22 05:11:32 +0200

    ajax call updated

--- public/js/ajax-call.js
+++ public/js/ajax-call.js
@@ -1,30 +1,38 @@
-function Login(email,passwd){
-    var Ruta ="/login";
+function Login(){
+    var Ruta ="/login_api";
     // pass auth vars to Security (Login) Controller
 
     //get form values
 
-    /*
-    var email = $('#email').val();
-    var passwd = $('#passwd').val();
-    */
+    
+    var email = $('#inputEmail').val();
+    var passwd = $('#inputPassword').val();
+    var token = $('#_csrf_token').val()
+    
     //({email: email,passwd: passwd}
 
     $.ajax({
          type: 'POST',
          url: Ruta,
-         data: $("#login_form").serialize(),
+         data: {email: email,password: passwd,_csrf_token:token},
          async: true,
          dataType:"json",
-         success: function(data, status) {  
-            //success example, modify template info
-            //$('..').append(e); $('#..').html('');
-
-            $('#login_info').html(data);  // data.username , data.passwd               
-            alert('Ajax login request!');
+         success: function(data) { 
+            if(data.status == "success"){
+               //hide form fields
+               $("#form-container").hide();
+               //show logged user
+               $('#login_info').html("Loggeado como <b>" + data.user + "</b>");  // data.username , data.passwd               
+            }else{
+               $('#login_info').html("<div class='error'>Datos incorrectos</div>");
+            }
+            
          },  
          error : function(xhr, textStatus, errorThrown) {  
-            alert('Ajax login request failed.');  
+            alert(errorThrown);
          }  
      });
+     
+
+     
 }
