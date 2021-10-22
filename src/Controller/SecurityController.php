96a1bddc6c5d80b3f7863de2b4f6203cde40fbc7
AppDeveloperV <vicmr1994@gmail.com>
2021-10-22 13:17:38 +0200

    ajax action updated

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,12 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Authenticator\Token;
+
+use Symfony\Component\Security\Core;
+
+
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SecurityController extends AbstractController
@@ -33,7 +40,10 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login_api", name="app_login_api")
      */
-    public function ajaxAction(Request $request,UserPasswordEncoderInterface $encoderService) : Response {  
+    public function ajaxAction(Request $request,UserPasswordEncoderInterface $encoderService,
+                                LoginFormAuthenticator $login_form_auth,AuthenticationUtils $authenticationUtils ) : Response {  
+        
+        //Token $post_auth
         
            
         if ($request->isXmlHttpRequest() ) {  
@@ -53,7 +63,15 @@ class SecurityController extends AbstractController
           
             if($user_db != null && $is_pass_valid != false){
 
-               $response = new JsonResponse(['user' => $user_db->getNombre(),'status' => 'success'] );
+                $session = $request->getSession();
+                $session->set('user', $user);
+
+                $login_form_auth->authenticate($request);
+
+                $firewallName = 'main';
+                //$auth = new $post_auth($user,$firewallName,$user_db->getRoles()); // trying to create token for session
+
+                $response = new JsonResponse(['user' => $user_db->getNombre(),'status' => 'success'] );
                
                return $response;
                
